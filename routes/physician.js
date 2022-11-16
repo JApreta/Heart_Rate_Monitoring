a107714b0dabd324d592fcf4e9c0f1ce0943c5ef
JAntonio <judithantonio@email.arizona.edu>
2022-11-16 12:46:14 -0700

    updating routes path

--- routes/physician.js
+++ routes/physician.js
@@ -11,18 +11,18 @@ router.get('/signup', function(req, res) {
 })
 
 
-router.post('/create', Physician_controller.create)
-router.post('/login', Physician_controller.login)
-router.get('/dashboard', protect, role('physician'), Physician_controller.dashboard)
+router.post('/api/create', Physician_controller.create)
+router.post('/api/login', Physician_controller.login)
+router.get('/api/dashboard', protect, role('physician'), Physician_controller.dashboard)
 
 //patient info endpoints
-router.get('/patient-list', protect, role('physician'), Patient_controller.patientList)
-    //router.get('/patient-detail/:email', protect, role('physician'), Patient_controller.patientDetail)
+router.get('/api/patient-list', protect, role('physician'), Patient_controller.patientList)
+    //router.get('/api/patient-detail/:email', protect, role('physician'), Patient_controller.patientDetail)
 
 
 //user update info endpoints
-//router.put('/update-password', protect, role('physician'), Patient_controller.passwordReset)
-//router.put('/update-user-info', protect, role('physician'), Patient_controller.updateUserInfo)
+//router.put('/api/update-password', protect, role('physician'), Patient_controller.passwordReset)
+//router.put('/api/update-user-info', protect, role('physician'), Patient_controller.updateUserInfo)
 
 
 
