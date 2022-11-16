d4468f3bf1b1ab5008d2324d842b38bb6f99c8ab
JAntonio <judithantonio@email.arizona.edu>
2022-11-16 14:08:57 -0700

    connecting front and back: patient signup inprogress...

--- public/js/patient.js
+++ public/js/patient.js
@@ -1,69 +1,73 @@
 const { $where } = require("../../models/user");
 
-function validate() {
+$("#signUpForm").submit(function(event) {
 
-    $('#signUpForm').submit();
+    event.preventDefault();
 
+    // data validation
+    if ($('#firstName').val() === "") {
+        window.alert("Invalid First Name!");
+        return;
+    }
+    if ($('#lastName').val() === "") {
+        window.alert("Invalid Last Name!");
+        return;
+    }
+    if ($('#email').val() === "") {
+        window.alert("Invalid Email!");
+        return;
+    }
 
-    // // data validation
-    // if ($('#fname').val() === "") {
-    //     window.alert("Invalid First Name!");
-    //     return;
-    // }
-    // if ($('#lname').val() === "") {
-    //     window.alert("Invalid Last Name!");
-    //     return;
-    // }
-    // if ($('#email').val() === "") {
-    //     window.alert("Invalid Email!");
-    //     return;
-    // }
+    // password validation
+    var pwd = $("#password").val();
+    if (!((pwd.length >= 10) && (pwd.length <= 20))) {
+        window.alert("Password must be between 10 and 20 characters!");
+        return;
+    }
+    if (!/[a-z]+/.test(pwd)) {
+        window.alert("Password must contain at least one lowercase character!");
+        return;
+    }
+    if (!/[A-Z]+/.test(pwd)) {
+        window.alert("Password must contain at least one uppercase character!");
+        return;
+    }
+    if (!/\d+/.test(pwd)) {
+        window.alert("Password must contain at least one digit!");
+        return;
+    }
+    if (pwd != $("#confirm_pwd").val()) {
+        window.alert("Passwords Don't Match!");
+        return;
+    }
+    var device = ('#device').val()
+    var token = ('#particle_token').val()
+    var userData = {
+        firstName: $('#firstName').val(),
+        lastName: $('#lastName').val(),
+        email: $('#email').val(),
+        password: pwd,
+        device_id: ('#device').val(),
+        particle_token: ('#particle_token').val(),
+    };
 
-    // // password validation
-    // var pwd = $("#pwd").val();
-    // if (!((pwd.length >= 10) && (pwd.length <= 20))) {
-    //     window.alert("Password must be between 10 and 20 characters!");
-    //     return;
-    // }
-    // if (!/[a-z]+/.test(pwd)) {
-    //     window.alert("Password must contain at least one lowercase character!");
-    //     return;
-    // }
-    // if (!/[A-Z]+/.test(pwd)) {
-    //     window.alert("Password must contain at least one uppercase character!");
-    //     return;
-    // }
-    // if (!/\d+/.test(pwd)) {
-    //     window.alert("Password must contain at least one digit!");
-    //     return;
-    // }
-    // if (pwd != $("#confirm_pwd").val()) {
-    //     window.alert("Passwords Don't Match!");
-    //     return;
-    // }
-    // var device = ('#device').val()
-    // var token = ('#particle_token').val()
+    // send new user details to server
+    $.ajax({
+            url: '/api/patient/create',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(userData),
+            dataType: 'json'
+        })
+        .done(function(data, textStatus, jqXHR) {
+            if (jqXHR.status == 201) {
+                alert(data.token)
+            } else {
+                alert(textStatus)
+            }
+        })
+        .fail(function(data, textStatus, jqXHR) {
 
-    // if (!validate_device_token(token, device)) {
-    //     window.alert("Incorrcet Device ID and/or Token!");
-    //     return;
-    // } 
-};
+        });
 
-// function validate_device_token(token, device) {
-
-//     var particle = new Particle();
-//     let validate = false
-
-//     var devicesPr = particle.getDevice({ deviceId: device, auth: token });
-
-//     devicesPr.then(
-//         function(data) {
-//             validate = true
-//         },
-//         function(err) {
-//             validate = false
-//         }
-//     );
-//     return validate
-// }
+});
