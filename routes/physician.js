168693cad691791186a8f89bbdd3e9aaa0388a2c
Teresa Pham <91837271+teresatpham@users.noreply.github.com>
2022-12-05 05:13:43 -0700

    update routes for physician portal

--- routes/physician.js
+++ routes/physician.js
@@ -7,18 +7,16 @@ const { protect, role } = require('../middleware/auth')
 
 
 
-router.post('/create', Physician_controller.create)
+router.post('/create', Physician_controller.create) // route to create new patient account
+router.get('/dashboard', protect, role('physician'), Physician_controller.dashboard) // route to get the physician's patients when entering the dashboard
 
-router.get('/dashboard', protect, role('physician'), Physician_controller.dashboard)
+router.get('/patient-list', protect, role('physician'), Physician_controller.getPatientList) // route to get the patient list
+router.get('/patient-device/:email', protect, role('physician'), Physician_controller.getPatientDevice) // route to get a patient's device(s)
+router.get('/patient-reading/:device', protect, role('physician'), Physician_controller.getPatientReading) // route to get readings from a device
 
-//patient info endpoints
-router.get('/patient-list', protect, role('physician'), Physician_controller.patientList)
-    //router.get('/patient-detail/:email', protect, role('physician'), Patient_controller.patientDetail)
-
-
-//user update info endpoints
-//router.put('/update-password', protect, role('physician'), Patient_controller.passwordReset)
-//router.put('/update-user-info', protect, role('physician'), Patient_controller.updateUserInfo)
+router.post('/measurment-frequency', protect, Physician_controller.updateMeasurmentFreq) // route to update the patient measurment frequency on particle io
+router.get('/patientSummary/:patient_email', protect, role('physician'), Patient_controller.weeklySummary) // route to get a patient's weeky reading summary
+router.get('/patientDetail/:patient_email', protect, Physician_controller.dailySummary) // route to get a patient's daily readings summary 
 
 
 
