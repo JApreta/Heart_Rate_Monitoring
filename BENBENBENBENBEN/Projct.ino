bf48d980ffab1150d09f746a7003d91160cb5aec
bencdouglas <70453519+bencdouglas@users.noreply.github.com>
2022-12-04 22:53:33 -0700

    Add files via upload

--- BENBENBENBENBEN/Projct.ino
+++ BENBENBENBENBEN/Projct.ino
@@ -1,33 +1,24 @@
 /*
-  MAX30105 Breakout: Output all the raw Red/IR/Green readings
-  By: Nathan Seidle @ SparkFun Electronics
-  Date: October 2nd, 2016
-  https://github.com/sparkfun/MAX30105_Breakout
+  513 Group Teresa Pham, Ben Douglas, Judith Antonio
 
-  Outputs all Red/IR/Green values.
 
-  Hardware Connections (Breakoutboard to Arduino):
-  -5V = 5V (3.3V is allowed)
-  -GND = GND
-  -SDA = A4 (or SDA)
-  -SCL = A5 (or SCL)
-  -INT = Not connected
-
-  The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
-  but it will also run at 3.3V.
-
-  This code is released under the [MIT License](http://opensource.org/licenses/MIT).
+  Uses code from ParticleIO examples as well as Sparkfun Libraries
 */
 
 #include <Wire.h>
+#include <cmath>
 #include "MAX30105.h"
 #include "heartRate.h"
 #include "spo2_algorithm.h"
 
+//Define System Mode to Allow non Wifi running
+SYSTEM_MODE(SEMI_AUTOMATIC);
+SYSTEM_THREAD(ENABLED);
+
+//Define Sensor for project
 MAX30105 particleSensor;
 
-#define debug Serial //Uncomment this line if you're using an Uno or ESP
-//#define debug SerialUSB //Uncomment this line if you're using a SAMD21
+#define debug Serial
 
 enum Stateys{standby, waitMeas, yesMeas};
 //Data Collection Variables
@@ -35,9 +26,10 @@ int cReddy = 0, oxy = 0, olDatar[1000], olDatao[1000],cNum = 0, lBeat = 0, cBeat
 //uploading bools
 bool jConnect = 1, isConnect = 0;
 //Timing Variables
-int stTimeh = 6,enTimeh =22,stTimem = 0,enTimem = 0, cTime = 0;
+int stTimeh = 6,enTimeh =22,stTimem = 0,enTimem = 0;
+int cTime = 0;
 //Variables you might want to change
-int betweenMeas = 30;//Set betweenMeas to the time you would like between Measurements(in seconds) 
+int betweenMeas = 30*60;//Set betweenMeas to the time you would like between Measurements(in seconds) 
 int lTime = betweenMeas * -1;//Set
 
 
@@ -46,16 +38,37 @@ int Rate3, oxxxy;
 String fullJson;
 
 Stateys cState = standby;
-LEDStatus mainBitch(RGB_COLOR_GREY, LED_PATTERN_FADE,LED_PRIORITY_NORMAL,LED_SOURCE_DEFAULT);
-LEDStatus wConnect(RGB_COLOR_YELLOW, LED_PATTERN_SOLID,LED_PRIORITY_BACKGROUND,LED_SOURCE_DEFAULT);
-LEDStatus sleepyTime(RGB_COLOR_RED, LED_PATTERN_BLINK,LED_PRIORITY_BACKGROUND,LED_SOURCE_DEFAULT);
-LEDStatus wMeasure(RGB_COLOR_BLUE, LED_PATTERN_BLINK,LED_PRIORITY_BACKGROUND,LED_SOURCE_DEFAULT);
-LEDStatus flashTime(RGB_COLOR_GREEN, LED_PATTERN_SOLID,LED_PRIORITY_BACKGROUND,LED_SOURCE_DEFAULT);
 
 void setup()
 {
     debug.begin(9600);
     debug.println("MAX30105 Basic Readings Example");
+    //Take control of LED
+    RGB.control(true);
+    //Establish Online set Variables
+    Particle.variable("Rate",Rate3);
+    Particle.variable("Oxxxy", oxxxy);
+    Particle.variable("FullJson", fullJson );
+    Particle.variable("stTimeh", stTimeh);
+    Particle.variable("enTimeh", enTimeh);
+    Particle.variable("stTimem",stTimem);
+    Particle.variable("enTimem",enTimem);
+    Particle.variable("betweenMeas",betweenMeas);
+    Particle.function("setEndUpdate", settyEnd);
+    Particle.function("setStartUpdate", settyStart);
+    Particle.function("setBetweenUpdate", settyDelay);
+
+
+    //Connect to Wifi
+    if (!Particle.connected()) {
+      debug.print("Attempting to Connect");
+      for(int i = 0; i<10;i++){
+        if (!Particle.connected()) {
+          lFlash(120,0,0,2);
+          delay(1000);
+          Particle.connect();
+      }}}
+
 
     // Initialize sensor
     if (particleSensor.begin() == false)
@@ -65,8 +78,8 @@ void setup()
         
     }
 
-    //SET LED speed
-     sleepyTime.setSpeed(LED_SPEED_FAST);
+  
+    
 
     //start sensor
     byte ledBrightness = 0xFF; //Options: 0=Off to 255=50mA
@@ -77,27 +90,29 @@ void setup()
     int adcRange = 16384; //Options: 2048, 4096, 8192, 16384
     particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
 
-    //Establish Online set Variables
-    Particle.variable("Rate",Rate3);
-    Particle.variable("Oxxxy", oxxxy);
-    Particle.variable("FullJson", fullJson );
+
+
+
 }
 
 
 void loop(){
-    switch(cState){
+   switch(cState){
+      //General waiting case while checking conditions for use
       case standby: 
-        if(1){
+        if(rightTime()){
           if(Time.now() > lTime+betweenMeas){
             cState = waitMeas;
             debug.print("\ntrans to waitMeas");
           }
         }
         break;
+      //Case for requesting measurement from user
       case waitMeas:
-        wMeasure.setPriority(LED_PRIORITY_IMPORTANT);
-        cTime = millis() + 1000*60*5;//set for the amount of time you want to wait between samples
-        while((!takeMeas(&cReddy, &oxy)) && (cTime > millis()));//holds until we have a sample or alloted time has passed
+        cTime = millis() + 1000*60*5;//set for the amount of time you want to wait until Timeout
+        while((!takeMeas(&cReddy, &oxy)) && (cTime > millis())){
+          lFlash(0,0,255,10);
+          RGB.color(0,0,255);}//holds until we have a sample or alloted time has passed
         cState = yesMeas;
         debug.print("\ntrans to yesMeas");
         if(cTime<millis()){//if we actually ran out of time:(
@@ -105,10 +120,9 @@ void loop(){
           lTime = Time.now();
           debug.print("\ntrans to standby");
         }
-        wMeasure.setPriority(LED_PRIORITY_BACKGROUND);
         break;
+        //Case for measurement found->sending to server
       case yesMeas:
-          //debug print in case of wifi issues
           debug.print("hr found! heartRate = ");
           debug.print(cReddy);
           debug.print("  spo2 = ");
@@ -118,29 +132,29 @@ void loop(){
 
           if(sendData(cReddy, oxy)){//if our data posts!
 
-            lFlash(RGB_COLOR_GREEN);
-            for(int i = 0; i <cNum; i++){
-              sendData(olDatar[cNum],olDatao[cNum]);
+            lFlash(0,255,0,3);
+            for(int i = 0; i <cNum; i++){//loop for sending any old data
+              sendData(olDatar[i],olDatao[i]);
               delay(1000);
+              lFlash(0,255,0,3);
             }
             cNum = 0;
           }else{
-            lFlash(RGB_COLOR_YELLOW);
+            lFlash(237, 234, 31,3);
+            delay(10);
           }
       cState = standby;
       debug.print("\ntrans to standby");
 
-    }
-    //debug.print("idk anymore");
-
+   }
 }
+
 
 bool takeMeas(int *reddy, int *oxxy){
   long int bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
   long int spo2, heartRate;
   uint32_t irBuffer[bufferLength], redBuffer[bufferLength];
   int8_t validSPO2, validHeartRate;
-
   //read the first 100 samples, and determine the signal range
   for (byte i = 0 ; i < bufferLength ; i++)
   {
@@ -165,54 +179,110 @@ bool takeMeas(int *reddy, int *oxxy){
     return 0;  
 }
 
-// bool gMeas(int r){
-//     //Set minThresh to expieremental values of no finger present
-//     int minThresh = 0;
-//     int maxThresh = 10000;
 
-//     if((r >= minThresh && r<= maxThresh))
-//         return 1;
-    
-//     return 0;
-// }
-
+//Checks to make sure our device is active at the right Timez
 bool rightTime(){
-    if((Time.hour()>=stTimeh)&&(Time.hour()>=stTimem)&&(Time.hour()<=enTimeh)&&(Time.minute()<=enTimem))
-        return 1;
+  //Convert from UTC to local time
+  int cHour = ((Time.hour()-7));
+  if(cHour<0)
+    cHour = cHour+24;
+  //cHour = cHour%24;
+    if((cHour>=stTimeh)&&(cHour<=enTimeh)){
+        if(cHour == stTimeh && Time.minute()<stTimem)
+          return 0;
+        if(cHour == enTimeh && Time.minute()>enTimem)
+          return 0;
+        return 1;}
     return 0;
 }
 
 
 
+
+
 //Flash the chosen color
-void lFlash(int color){
-    //Flash the chosen color for .1 before returning to the original color;
-    mainBitch.setColor(color);
-    mainBitch.setPattern(LED_PATTERN_SOLID);
+void lFlash(int red, int green, int blue, int loops){
+    for(int i =0; i < loops; i++){
+    RGB.color(red, green, blue);
     delay(100);
-    mainBitch.setColor(RGB_COLOR_GREEN);
-    mainBitch.setPattern(LED_PATTERN_FADE);
+    RGB.color(0,0,0);
+    delay(100);}
+
 }
 
 
 //Sending Function
 bool sendData(int r, int o){
+    //Attempt to Reconnect to Wifi
+    if (!Particle.connected()) {
+      debug.print("Attempting to Connect");
+      for(int i = 0; i<10;i++){
+        if (!Particle.connected()) {
+          lFlash(120,0,0,2);
+          delay(1000);
+          Particle.connect();
+      }}}
+    //Create JSON to send through webhook
     Rate3 = r;
     oxxxy = o;
-    fullJson = String::format("{Rate\":%d,\"Oxy\":%d,\"Time\":%d:%d:%d, \"Date\":%d\\%d\\%d}", r, o, Time.hour(),Time.minute(),Time.second(),Time.month(), Time.day(),Time.year());
-    //publish wil return 0 if send is not successful, return back to main in order to catch errors:0
-    if(!Particle.publish("testEvent", String::format("{\"Rate\":%d,\"Oxy\":%d,\"Time\":%d}", r, o, Time.now()))){
+    fullJson = String::format("{\"rate\":\"%d\",\"oxy\":\"%d\",\"hour\":\"%d%\",\"minute\":\"%d\",\"month\":\"%d\",\"day\":\"%d\",\"year\":\"%d\"}", r, o, Time.hour(),Time.minute(),Time.month(), Time.day(),Time.year());
+    //publish wil return 0 if send is not successful, which we can use to tell if we need to store in our array to send l8r
+    if(!Particle.publish("toJudith", fullJson)){
       olDatar[cNum] = r;
-      cNum++;
       olDatao[cNum] = o;
+      cNum++;
       return 0;}
     delay(1000);
-    // fullJson = String::format("{\"Rate\":%d,\"Oxy\":%d,\"Time\":%d:%d:%d, \"Date\":%d\\%d\\%d}", r, o, Time.hour(),Time.minute(),Time.second(),Time.month(), Time.day(),Time.year());
-   
     Serial.print("\njust sent");
     return 1;
 
 
 
 
+}
+
+//sets the delay from post request
+int settyDelay(String lesgovalues){
+  JSONValue lesgovaluesp = JSONValue::parseCopy(lesgovalues);
+  JSONObjectIterator iter(lesgovaluesp);
+
+  //iterates through our(one line) JSON file and sets the between measurements variable
+  while (iter.next())
+    {
+        if (iter.name() == "delayokay"){
+          betweenMeas = iter.value().toInt()*60;}
+    }
+    lFlash(135, 26, 132,3);
+  return 1;
+
+}
+
+int settyEnd(String lesgovalues){
+  JSONValue lesgovaluesp = JSONValue::parseCopy(lesgovalues);
+  JSONObjectIterator iter(lesgovaluesp);
+  //iterates through and sets hour and minute for starting
+  while (iter.next())
+    {
+        if (iter.name() == "hour"){
+          enTimeh = iter.value().toInt();}
+        if (iter.name() == "minute")
+          enTimem = iter.value().toInt();
+    }
+    lFlash(135, 26, 132, 3);
+  return 1;
+}
+
+int settyStart(String lesgovalues){
+  JSONValue lesgovaluesp = JSONValue::parseCopy(lesgovalues);
+  JSONObjectIterator iter(lesgovaluesp);
+  //iterates through and sets hour and minute for ending
+  while (iter.next())
+    {
+        if (iter.name() == "hour"){
+          stTimeh = iter.value().toInt();}
+        if (iter.name() == "minute")
+          stTimem = iter.value().toInt();
+    }
+    lFlash(135, 26, 132,3);
+  return 1;
 }
